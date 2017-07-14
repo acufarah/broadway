adbb91dccdfce31feba4220fcbcb695b42980b58
Farah Khan <farahkhanu@gmail.com>
2017-07-14 04:51:48 +0000

    Edit and delete actions and view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -20,6 +20,22 @@ class PlaysController < ApplicationController
   end
   
   def show
+  end 
+  
+  def edit 
+  end
+  
+  def update
+    if @play.update(play_params)
+      redirect_to play_path(@play)
+    else
+      render 'edit'
+    end
+  end  
+    
+  def destroy
+    @play.destroy
+    redirect_to root_path
   end  
   
   private
